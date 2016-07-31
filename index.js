044ac1c8960d13eb72401456c37a8db6a93b85db
Michael Mitchell-Halter <0x4d464d48@gmail.com>
2016-07-31 05:52:09 -0700

    Cleaned up some files before test deploy

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+var port = parseInt(process.argv[2]);
+
+if(port === undefined || typeof(parseInt(process.argv[0])) !== "number")
+{
+  process.exit();
+}
+
 app.use(express.static('public'));
 app.use(express.static('.'));
 
-app.get('/', function(req, res) {
-     res.sendFile(path.join(__dirname + '/public/index.html'));
-     console.log('zing!');
-     console.log(path.join(__dirname + '/public/index.html'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+  console.log('zing!');
+  console.log(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/login', function(req, res) {
-     res.sendFile(path.join(__dirname + '/public/compiled_app/login/login.component.html'));
-     console.log('HEY!');
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/compiled_app/login/login.component.html'));
 });
 
-app.listen(8080, function() {
-    console.log('Server running');
-    console.log(__dirname);
+app.listen(port, function () {
+  console.log('Server running on port ' + port.toString());
 });
