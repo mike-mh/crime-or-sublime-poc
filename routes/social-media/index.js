3501bf926a95e00b5fc5636d8bef79c832833972
Michael Mitchell-Halter <0x4d464d48@gmail.com>
2016-09-04 04:28:04 -0700

    added reddit routes to main router

--- routes/social-media/index.js
+++ routes/social-media/index.js
@@ -2,9 +2,11 @@
 
 let addTwitterRoutes = require('./twitter');
 let addFacebookRoutes = require('./facebook');
+let addRedditRoutes = require('./reddit');
 
 module.exports = function(router)
 {
   addTwitterRoutes(router);
   addFacebookRoutes(router);
+  addRedditRoutes(router);
 }
