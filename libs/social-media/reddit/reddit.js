2b01a541813a30f8bab3041a361c40f012ab5414
Michael Mitchell-Halter <0x4d464d48@gmail.com>
2016-09-04 17:12:46 -0700

    Should now make a post to reddit

--- libs/social-media/reddit/reddit.js
+++ libs/social-media/reddit/reddit.js
@@ -7,6 +7,7 @@ const REDDIT_URL = 'https://www.reddit.com';
 
 const REDDIT_AUTHORIZE_PATH = '/api/v1/authorize';
 const REDDIT_ACCESS_TOKEN_PATH = '/api/v1/access_token';
+const REDDIT_SUBMIT_PATH = '/api/submit';
 
 const REDDIT_PERMISSIONS = ['submit', 'identity', 'history'];
 
@@ -116,8 +117,52 @@ function associateAccessTokenWithSession(code, session) {
   return getAccessTokenPromise;
 }
 
+/**
+ * Use this function to generate a thread on Reddit. Unsure if underscore in
+ * '_request' is meant to signify that the method is supposed to be private
+ * but will consider making a pull request to their library to change that...
+ * although a public version of that method would probably do the exact same
+ * thing.
+ * 
+ * @param session {obsject} - Session associated with user.
+ * 
+ * @return {Promise} - Promise resolves to error message should one occur._customHeaders
+ *   Otherwise simply resolves.
+ */
+function postToReddit(session) {
+  let client = generateRedditOAuthClient();
+
+  // It's a bit smelly to do it this way but library doesn't support a basic
+  // post method yet.
+  let postObject = {
+    title: 'Hello Reddit!',
+    text: 'Yay!',
+    sr: 'sircmpwn',
+    kind: 'self',
+  }
+
+  let postBody = querystring.stringify(postObject);
+  let submitRedditThreadPromise = new Promise((resolve, rejects) => {
+    client._request(
+      "POST",
+      REDDIT_URL + REDDIT_SUBMIT_PATH,
+      null,
+      postBody,
+      session.redditAccessToken,
+      (error, data) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(data);
+      });
+  });
+
+  return submitRedditThreadPromise;
+}
+
 RedditClient.getRedditLoginUrl = getRedditLoginUrl;
 RedditClient
   .associateAccessTokenWithSession = associateAccessTokenWithSession;
+RedditClient.postToReddit = postToReddit;
 
 module.exports = RedditClient;
--- routes/social-media/reddit.js
+++ routes/social-media/reddit.js
@@ -4,6 +4,7 @@ let RedditClient = require('../../libs/social-media/reddit/reddit');
 
 const REDDIT_LOGIN_URL = '/reddit-login';
 const REDDIT_REDIRECT_PATH = '/reddit-redirect';
+const REDDIT_CREATE_THREAD_PATH = '/reddit-create-thread'
 const REDDIT_REDIRECT_URI = 'https://crime-or-sublime.herokuapp.com' +
                             REDDIT_REDIRECT_PATH;
 
@@ -16,11 +17,19 @@ function redditRetrieveAccessToken(req, res) {
   let code = req.query.code;
   RedditClient
     .associateAccessTokenWithSession(code, req.session)
-      .then(() => { res.send(req.session); })
+      .then(() => { res.send('YOU WIN!'); })
       .catch((error) => { res.send(error); });
+}
+
+function postRedditThread(req, res) {
+  RedditClient
+    .postToReddit()
+      .then((data) => res.send(data))
+      .catch((error) => {res.send(error)});
 }
 
 module.exports = function(router) {
   router.get(REDDIT_LOGIN_URL, redditLogin);
   router.get(REDDIT_REDIRECT_PATH, redditRetrieveAccessToken);
+  router.get(REDDIT_CREATE_THREAD_PATH, postRedditThread);
 }
