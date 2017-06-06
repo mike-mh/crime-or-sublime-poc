91e09eab49ecec6381e590289eaca130e73fd992
Michael Mitchell-Halter <w9g8@ugrad.cs.ubc.ca>
2017-06-06 07:06:22 -0700

    Added the rate front end stub

---
+++ src/public/app/rate/rate.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy } from "@angular/core";
+import { Observer } from "rxjs/Observer";
+import { SubjectSubscription } from "rxjs/SubjectSubscription";
+import { ISessionDetails, SessionService } from "../shared/session/session.service";
+
+@Component({
+    providers: [SessionService],
+    selector: "rate",
+    styleUrls: ["./rate.component.css"],
+    templateUrl: "./rate.component.html",
+})
+
+/**
+ * Contains the navbar component. Doesn't have any business logic yet.
+ */
+export class RateComponent implements OnDestroy {
+    public isLoggedIn: boolean = false;
+    private sessionStatus: SubjectSubscription<ISessionDetails>;
+
+    private sessionUpdateCallback: Observer<ISessionDetails> = {
+        complete: null,
+        error: null,
+        // This is the function that gets executed during a status update.
+        next: (response) => {
+            if (response.error) {
+                return;
+            }
+            this.isLoggedIn = (response.email) ? true : false;
+        },
+    };
+
+    constructor(private sessionService: SessionService) {
+        this.sessionStatus = new SubjectSubscription(SessionService.sessionStatusEmitter, this.sessionUpdateCallback);
+        SessionService.sessionStatusEmitter.subscribe(this.sessionUpdateCallback);
+        this.isLoggedIn = SessionService.isSessionActive();
+    }
+
+    /**
+     * Need to unsubscribe from the session emitter.
+     */
+    public ngOnDestroy(): void {
+        // Manually remove observer from event emitter. Unsubscribe doesn't work.
+        // Will need to come back to this and fix it.
+        const observerIndex = SessionService.sessionStatusEmitter.observers.indexOf(this.sessionUpdateCallback);
+        SessionService.sessionStatusEmitter.observers.splice(observerIndex, 1);
+        this.sessionStatus.unsubscribe();
+    }
+
+}
---
+++ src/public/app/rate/rate.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { RateComponent } from "./rate.component";
+
+@NgModule({
+    bootstrap: [RateComponent],
+    declarations: [RateComponent],
+    entryComponents: [RateComponent],
+    exports: [RateComponent],
+    imports: [CommonModule],
+})
+
+export class RateModule {
+
+}
