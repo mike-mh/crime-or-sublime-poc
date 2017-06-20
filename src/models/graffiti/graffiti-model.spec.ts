d95d00b719ffc7d13518a008380ded3dfe294f79
Michael Mitchell-Halter <w9g8@ugrad.cs.ubc.ca>
2017-06-20 01:17:19 -0700

    tests written for graffiti-submit router and temp-graffiti model

--- src/models/graffiti/graffiti-model.spec.ts
+++ src/models/graffiti/graffiti-model.spec.ts
@@ -3,6 +3,7 @@ import { Document } from "mongoose";
 import { Observable } from "rxjs/Observable";
 import { CoSServerConstants } from "../../cos-server-constants";
 import { GraffitiModel } from "./graffiti-model";
+import { TempGraffitiModel } from "./temp-graffiti-model";
 
 describe("GraffitiModel", () => {
     const graffitiModel: any = new GraffitiModel();
@@ -117,4 +118,93 @@ describe("GraffitiModel", () => {
             });
     });
 
+});
+
+describe("GraffitiTempModel", () => {
+    const tempGraffitiModel: any = new TempGraffitiModel();
+    const graffitiModel: any = new GraffitiModel();
+
+    beforeAll((done) => {
+        graffitiModel.saveDocument({
+            latitude: 1,
+            longitude: 2,
+            url: "raboogs",
+        }).flatMap(() => {
+            return tempGraffitiModel.saveDocument({
+                latitude: 1,
+                longitude: 2,
+                url: "sgoobar",
+            });
+        }).subscribe(done);
+    });
+
+    afterAll((done) => {
+        graffitiModel.removeDocuments({})
+            .flatMap(() => {
+                return tempGraffitiModel.removeDocuments({})
+            })
+            .subscribe(done);
+    });
+
+    it("should be able to recognize when a graffiti URL is taken by a tempgraffiti", (done) => {
+        tempGraffitiModel.ensureGraffitiIsUnique("sgoobar").subscribe(
+            () => {
+                expect(true).toBe(false, "Was able to save a graffiti with taken URL.");
+                done();
+            },
+            (error: any) => {
+                expect(error.code).toEqual(CoSServerConstants.DATABASE_GRAFFITI_ALREADY_REGISTERED_ERROR.code);
+                done();
+            });
+    });
+
+    it("should be able to recognize when a graffiti URL is taken by a graffiti", (done) => {
+        tempGraffitiModel.ensureGraffitiIsUnique("sgoobar").subscribe(
+            () => {
+                expect(true).toBe(false, "Was able to save a graffiti with taken URL.");
+                done();
+            },
+            (error: any) => {
+                expect(error.code).toEqual(CoSServerConstants.DATABASE_GRAFFITI_ALREADY_REGISTERED_ERROR.code);
+                done();
+            });
+    });
+
+    it("should not save a tempgraffiti with a url taken by a tempgraffiti", (done) => {
+        tempGraffitiModel.commitTempGraffitiData("sgoobar", 1, 2).subscribe(
+            () => {
+                expect(true).toBe(false, "Was able to save a graffiti with taken URL.");
+                done();
+            },
+            (error: any) => {
+                expect(error.code).toEqual(CoSServerConstants.DATABASE_GRAFFITI_ALREADY_REGISTERED_ERROR.code);
+                done();
+            });
+    });
+
+    it("should not save a tempgraffiti with a url taken by a graffiti", (done) => {
+        tempGraffitiModel.commitTempGraffitiData("raboogs", 1, 2).subscribe(
+            () => {
+                expect(true).toBe(false, "Was able to save a graffiti with taken URL.");
+                done();
+            },
+            (error: any) => {
+                expect(error.code).toEqual(CoSServerConstants.DATABASE_GRAFFITI_ALREADY_REGISTERED_ERROR.code);
+                done();
+            });
+    });
+
+    it("should save a tempgraffiti with appropriate data to the database", (done) => {
+        tempGraffitiModel.commitTempGraffitiData("new", 1, 2).subscribe(
+            (document: any) => {
+                expect(document.url).toBe("new", "Returned document was incorrect.");
+                done();
+            },
+            (error: any) => {
+                expect(true).toBe(false, "Error occured saving document.");
+                done();
+            });
+    });
+
+    // TO-DO: Implement migration of temp graffiti to permenant graffiti
 });
---
+++ src/routes/graffiti/graffiti-submit/graffiti-submit-router.spec.ts
@@ -0,0 +1,137 @@
+import { Router } from "express";
+import { request } from "https";
+import { GraffitiSubmitAPI } from "../../../../configurations/graffiti/graffiti-submit/graffiti-submit-api";
+import { CoSServer } from "../../../cos-server";
+import { CoSServerConstants } from "../../../cos-server-constants";
+import { GraffitiModel } from "../../../models/graffiti/graffiti-model";
+import { GraffitiSubmitRouter } from "./graffiti-submit-router";
+
+describe("GraffitiSubmitRouter", () => {
+    const graffitiSubmitAPI: GraffitiSubmitAPI = new GraffitiSubmitAPI();
+    const graffitiSubmitRouter: any = new GraffitiSubmitRouter(Router());
+
+    const req: any = {
+        body: {},
+        method: "get",
+        params: {},
+        session: {
+            destroy: () => { return; },
+        },
+    };
+
+    const res: any = {
+        json: () => { return; },
+    };
+
+    it("should have a handler installed for the graffiti-submit-new-submission path", () => {
+        const stacks = graffitiSubmitRouter.getRouter().stack.filter((layer: any) => {
+            return layer.route.path === "/graffiti-submit-new-submission";
+        });
+        expect(stacks.length).toEqual(1, "URL was not stored to router correctly.");
+    });
+
+    it("should reject requests to submit graffiti without parameters", (done) => {
+        req.method = "post";
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+    it("should reject requests to submit graffiti without an id in it's parameters", (done) => {
+        req.method = "post";
+        req.body = {
+            latitude: 100,
+            longitude: 100,
+            recaptcha: "deadbeef",
+        }
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+    it("should reject requests to submit graffiti without a latitude in it's parameters", (done) => {
+        req.method = "post";
+        req.body = {
+            id: "deadbeef",
+            longitude: 100,
+            recaptcha: "deadbeef",
+        }
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+    it("should reject requests to submit graffiti without a longitude in it's parameters", (done) => {
+        req.method = "post";
+        req.body = {
+            id: "deadbeef",
+            latitude: 100,
+            recaptcha: "deadbeef",
+        }
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+    it("should reject requests to submit graffiti without a recaptcha in it's parameters", (done) => {
+        req.method = "post";
+        req.body = {
+            id: "deadbeef",
+            latitude: 100,
+            longitude: 100,
+        }
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+    it("should reject requests to submit graffiti that aren't made with POST", (done) => {
+        req.method = "get";
+        req.body = {
+            id: "deadbeef",
+            latitude: 100,
+            longitude: 100,
+            recaptcha: "deadbeef",
+        }
+
+        res.json = (response: any) => {
+            expect(response.error.name).toEqual(graffitiSubmitAPI.responses.InvalidParametersError.error.name);
+            // Reset response function
+            res.json = () => { return; };
+            done();
+        };
+
+        graffitiSubmitRouter.graffitiSubmitNewSubmission(req, res);
+    });
+
+});
